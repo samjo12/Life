06e2900e56889ba1c1fce197af8fc33277a2bafd
Aleksei Sadikov <79467164+samjo12@users.noreply.github.com>
2023-02-27 13:03:19 +0300

    Initial commit

---
+++ Class_teach/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Class_teach
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.individ_count_label = new System.Windows.Forms.Label();
+            this.family_count_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // individ_count_label
+            // 
+            this.individ_count_label.AutoSize = true;
+            this.individ_count_label.Location = new System.Drawing.Point(7, 9);
+            this.individ_count_label.Name = "individ_count_label";
+            this.individ_count_label.Size = new System.Drawing.Size(71, 15);
+            this.individ_count_label.TabIndex = 2;
+            this.individ_count_label.Text = "Индивидов:";
+            // 
+            // family_count_label
+            // 
+            this.family_count_label.AutoSize = true;
+            this.family_count_label.Location = new System.Drawing.Point(7, 50);
+            this.family_count_label.Name = "family_count_label";
+            this.family_count_label.Size = new System.Drawing.Size(46, 15);
+            this.family_count_label.TabIndex = 3;
+            this.family_count_label.Text = "Семей:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1784, 961);
+            this.Controls.Add(this.family_count_label);
+            this.Controls.Add(this.individ_count_label);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label individ_count_label;
+        private System.Windows.Forms.Label family_count_label;
+    }
+}
+
---
+++ Class_teach/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+//using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Class_teach
+{
+    public partial class Form1 : Form
+    {
+        int count=0;            
+        Timer timer1 = new Timer();
+        public static Random rand1 = new Random(1);
+       // bool flag_life=false; //Флаг, где true- идет жизнь, false - ждем таймера
+        public static Graphics GR=null;
+        public static Bitmap BM=null;
+        List<Men> pop_base= new List<Men>();
+        List<Family> fam_base = new List <Family>();
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            GR = Graphics.FromHwnd(Handle);
+  
+
+        }
+
+        public void init_field()
+        {
+            GR.Clear(Color.Green); 
+
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                Men.new(null); count++;
+
+                this.Location = new Point(this.Location.X - 5, this.Location.Y - 5);
+                
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            init_field();
+            Men new_man = new Men(null); // появился какой-то совершеннолетний тип случайного пола без фамилии
+            count++;
+        }
+        private void start_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            timer1.Interval = 1000; // 1 сек
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            stop_Click(sender,e);
+            // Начинаем жить если ожидали таймера, если уже живем - то пропускаем тик.
+           // if (flag_life is true) return; else flag_life = true;
+            Do_Life();
+            start_Click(sender, e);
+        }
+        public void Do_Life()
+        {
+            // нужно перебрать всех людей и сделать ими ход
+            //flag_life = false;
+            return;
+        }
+
+
+        public class Family
+        {
+            int R; // цвета семьи
+            int B;
+            int G;
+            static int count=0; // количество человек в семье
+
+            public void New(Men Rod1,Men Rod2) //создаем новую семью
+            {
+                using (GR = Graphics.FromImage(BM))
+                {
+
+                    Pen fam_pen = new Pen(Color.FromArgb(0, 0, 0), 1);
+                    SolidBrush fam_brush = new SolidBrush(Color.Blue);
+                    GR.FillEllipse(fam_brush, 300, 240, 10, 10);
+                }
+            }
+            public void Dispose() // распад семьи
+            {
+
+            }
+
+        }
+        public class Men
+        {
+            bool sex;
+            Family my_family;
+            int age; // возраст
+            int x;   // координата X
+            int y;  // координата Y
+            int R;  // цвета индивида с рождения
+            int B;
+            int G;
+            int force; //сила индивида
+            int wel; //здоровье индивида
+            int fert; //фертильность индивида
+
+            public Men(Family new_fam)
+            {
+                this.sex =  Convert.ToBoolean(rand1.Next(0, 1));
+                this.my_family = new_fam;
+                if (new_fam is null) this.age = 18; else this.age = 0;
+               
+
+                    //Graphics g = pictureBox1.CreateGraphics();
+
+                    // Создаем объекты-кисти для закрашивания фигур
+                    //Выбираем перо myPen желтого цвета толщиной в 1 пикселя:
+                    //myGrid.Background = brush;
+                    Pen fam_pen = new Pen(Color.FromArgb(0, 0, 0), 1);
+
+                    // g.DrawEllipse(fam_pen, 300, 240, 10, 10); // окружностей
+                    SolidBrush fam_brush = new SolidBrush(Color.Blue);
+                    GR.FillEllipse(fam_brush, 300, 240, 10, 10);
+                
+            }
+
+        }
+
+
+    }
+}
+// Закрашиваем фигуры
+/*
+SolidBrush myCorp = new SolidBrush(Color.DarkMagenta);
+SolidBrush myTrum = new SolidBrush(Color.DarkOrchid);
+SolidBrush myTrub = new SolidBrush(Color.DeepPink);
+SolidBrush mySeа = new SolidBrush(Color.Blue);
+ g.FillRectangle(myTrub, 300, 125, 75, 75); // 1 труба (прямоугольник)
+ g.FillRectangle(myTrub, 480, 125, 75, 75); // 2 труба (прямоугольник)
+ g.FillPolygon(myCorp, new Point[]      // корпус (трапеция)
+   {
+     new Point(100,300),new Point(700,300),
+     new Point(700,300),new Point(600,400),
+     new Point(600,400),new Point(200,400),
+     new Point(200,400),new Point(100,300)
+   }
+ );
+ g.FillRectangle(myTrum, 250, 200, 350, 100); // палуба (прямоугольник)
+                                              // Море - 12 секторов-полуокружностей
+
+ int x = 50;
+ int Radius = 50;
+ while (x <= pictureBox1.Width - Radius)
+ {
+     g.FillPie(mySeа, 0 + x, 375, 50, 50, 0, -180);
+     x += 50;
+ }*/
+// Иллюминаторы 
